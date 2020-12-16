f9f6573c5896490e5974c5321b94a515eca834c3
progfay <shun1856shun@gmail.com>
2020-12-16 23:57:50 +0900

    feat: impl

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 const CDP = require('chrome-remote-interface')
 
+const urlList = ['http://localhost:8000']
+
 const main = async () => {
-  const url = 'http://localhost:8000'
   const client = await CDP({
     port: 9222,
     host: process.env.CHROME_HOST ?? 'localhost',
   })
-  await client.Log.entryAdded(console.log)
+
+  let url = ''
+  const issues = []
+
+  await client.Log.entryAdded(entry => {
+    issues.push({ type: 'log', url, entry })
+  })
   await client.Log.enable()
+  await client.Audits.issueAdded(issue => {
+    issues.push({ type: 'log', url, issue })
+  })
+  await client.Audits.enable()
   await client.Page.enable()
 
-  await client.Page.navigate({ url })
-  await client.Page.loadEventFired()
+  for (let i = 0; i < urlList.length; i++) {
+    url = urlList[i]
+    await client.Page.navigate({ url })
+    await client.Page.loadEventFired()
+  }
 
   await client.Log.disable()
   await client.Page.disable()
   await client.close()
+
+  console.log(JSON.stringify(issues, undefined, 2))
 }
 
 main().catch(console.error)
